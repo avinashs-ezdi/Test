d01771f7ad9514d7eefc63b04ca3825d3c876d59
EZDI\avinash.m <avinash.s@ezdi.us>
2015-12-02 19:05:31 +0530

    aa

---
+++ src/test/java/MavenOnGitTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MavenOnGitTest {
+
+@Test
+    public void simpleTest() {
+
+        String str = "Hello its me";
+
+        assertEquals("Hello its me", str);
+
+
+    }
+
+
+}
